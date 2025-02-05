349f9c1cc6a68b18af680008cc3e703149554efe
kamakshi <kamakshipandoh8@gmail.com>
2025-02-06 02:13:52 +0530

    updated dashboard

--- front-end/src/partials/dashboard/Sentiment.jsx
+++ front-end/src/partials/dashboard/Sentiment.jsx
@@ -7,21 +7,20 @@ import { getCssVariable } from '../../utils/Utils';
 function DashboardCard06() {
 
   const chartData = {
-    labels: ['United States', 'Italy', 'Other'],
+    labels: ['Positive', 'Negative', 'Neutral'],
     datasets: [
       {
-        label: 'Top Countries',
         data: [
           35, 30, 35,
         ],
         backgroundColor: [
-          getCssVariable('--color-violet-500'),
-          getCssVariable('--color-sky-500'),
+          getCssVariable('--color-green-300'),
+          getCssVariable('--color-red-300'),
           getCssVariable('--color-violet-800'),
         ],
         hoverBackgroundColor: [
-          getCssVariable('--color-violet-600'),
-          getCssVariable('--color-sky-600'),
+          getCssVariable('--color-green-500'),
+          getCssVariable('--color-red-500'),
           getCssVariable('--color-violet-900'),
         ],
         borderWidth: 0,
@@ -32,10 +31,8 @@ function DashboardCard06() {
   return (
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-gray-800 shadow-xs rounded-xl">
       <header className="px-5 py-4 border-b border-gray-100 dark:border-gray-700/60">
-        <h2 className="font-semibold text-gray-800 dark:text-gray-100">Top Countries</h2>
+        <h2 className="font-semibold text-gray-800 dark:text-gray-100">Overall sentiment distribution</h2>
       </header>
-      {/* Chart built with Chart.js 3 */}
-      {/* Change the height attribute to adjust the chart height */}
       <DoughnutChart data={chartData} width={389} height={260} />
     </div>
   );
